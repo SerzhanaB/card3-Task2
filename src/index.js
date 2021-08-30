5aca198d8e5e503f4920298b13b2bf851bb3057d
Serzhana <buraevase@gmail.com>
2021-08-31 00:58:21 +0300

    Первый коммит

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+/*
+// Способ 1
+//Если Время считать через стрелочную функцию - все работает
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>Локальное время {new Date().toLocaleTimeString()}</div>,
+  document.getElementById("root")
+);
+const Locktime = () => (
+  <div>Локальное время {new Date().toLocaleTimeString()}</div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(Tick, 30000);
+function Tick() {
+  ReactDOM.render(<Locktime />, document.getElementById("root"));
+}
+*/
+
+ // Способ 2 просто обычный элемент
+ReactDOM.render(
+  <div>Локальное время {new Date().toLocaleTimeString()}</div>,  
+  document.getElementById('root'));
+
+setInterval(Tick,30000);
+function Tick(){
+  const Locktime= <div>Локальное время {new Date().toLocaleTimeString()}</div>;
+
+  ReactDOM.render(
+    Locktime,  
+    document.getElementById('root')
+  );
+  }
+ 
